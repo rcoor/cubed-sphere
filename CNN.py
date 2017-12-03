5d47468c6ae6fd702e2585d5ad08943a923f26db
Thorn Hjalte Thorn Jakobsen <thornjakobsen@gmail.com>
2017-12-03 18:44:36 +0100

    added changes

--- CNN.py
+++ CNN.py
@@ -100,30 +100,32 @@ class CNNModel(object):
     def _conv_layer(self, input,
                     channels_in,
                     channels_out,
-                    name="conv",
                     ksize_r=3,
                     ksize_xi=3,
                     ksize_eta=3,
                     stride_r=2,
                     stride_xi=2,
-                    stride_eta=2):
+                    stride_eta=2,
+                    name="conv"):
         with tf.variable_scope(name) as scope:
-            W = self._weight_variable("weights", [channels_in, channels_out])
+            W = self._weight_variable("weights", [ksize_r, ksize_xi, ksize_eta, channels_in, channels_out])
             b = tf.get_variable(
                 "b", shape=[channels_out], initializer=self.bias_initializer, dtype=tf.float32)
-            convolution = conv_spherical_cubed_sphere(
-                input, W, strides=[1, stride_r, stride_xi, stride_eta, 1], padding="VALID", name=name)
+            convolution = conv_spherical_cubed_sphere(input, W, strides=[
+                1, stride_r, stride_xi, stride_eta, 1],
+                padding="VALID",
+                name=name)
             return tf.nn.relu(convolution + b)
 
     def _pooling_layer(self,
                        input,
-                       name="pooling",
                        ksize_r=3,
                        ksize_xi=3,
                        ksize_eta=3,
                        stride_r=2,
                        stride_xi=2,
-                       stride_eta=2):
+                       stride_eta=2,
+                       name="pooling"):
         with tf.variable_scope(name) as scope:
             return avg_pool_spherical_cubed_sphere(input, ksize=[1, ksize_r, ksize_xi, ksize_eta, 1], strides=[1, stride_r, stride_xi, stride_eta, 1], padding="VALID")
 
@@ -131,22 +133,94 @@ class CNNModel(object):
         # Reshape to use within a convolutional neural net.
         x = tf.reshape(self.x, shape=[-1, 6, 24, 38, 38, 2])
 
-        conv1 = self._conv_layer(x, 2, 16,  name="conv1")
-        pool1 = self._pooling_layer(conv1)
+        ### LAYER 1 ###
+        conv1 = self._conv_layer(x,
+                                 ksize_r=3,
+                                 ksize_xi=5,
+                                 ksize_eta=5,
+                                 channels_out=16,
+                                 channels_in=2,
+                                 stride_r=1,
+                                 stride_xi=2,
+                                 stride_eta=2,
+                                 name="conv1")
 
-        conv2 = self._conv_layer(conv1, 16, 32,  name="conv2")
+        pool1 = self._pooling_layer(conv1,
+                                    ksize_r=1,
+                                    ksize_xi=3,
+                                    ksize_eta=3,
+                                    stride_r=1,
+                                    stride_xi=2,
+                                    stride_eta=2)
 
-        conv3 = self._conv_layer(conv2, 32, 64,  name="conv3")
+        ### LAYER 2 ####
+        conv2 = self._conv_layer(pool1, ksize_r=3,
+                                 ksize_xi=3,
+                                 ksize_eta=3,
+                                 channels_in=16,
+                                 channels_out=32,
+                                 stride_r=1,
+                                 stride_xi=1,
+                                 stride_eta=1,  name="conv2")
 
-        conv4 = self._conv_layer(conv3, 64, 128,  name="conv4")
+        pool2 = self._pooling_layer(conv2,
+                                    ksize_r=3,
+                                    ksize_xi=3,
+                                    ksize_eta=3,
+                                    stride_r=2,
+                                    stride_xi=2,
+                                    stride_eta=2, name="pool2")
 
-        flattened = tf.reshape(conv4,  [-1, self._reduce_dim(conv4)])
+        ### LAYER 3 ####
+        conv3 = self._conv_layer(pool2,
+                                 ksize_r=3,
+                                 ksize_xi=3,
+                                 ksize_eta=3,
+                                 channels_in=32,
+                                 channels_out=64,
+                                 stride_r=1,
+                                 stride_xi=1,
+                                 stride_eta=1,  name="conv3")
+
+        pool3 = self._pooling_layer(conv3,
+                                    ksize_r=1,
+                                    ksize_xi=3,
+                                    ksize_eta=3,
+                                    stride_r=1,
+                                    stride_xi=2,
+                                    stride_eta=2, name="pool3")
+
+        ### LAYER 4 ####
+        conv4 = self._conv_layer(pool3,
+                                 ksize_r=3,
+                                 ksize_xi=3,
+                                 ksize_eta=3,
+                                 channels_in=64,
+                                 channels_out=128,
+                                 stride_r=1,
+                                 stride_xi=1,
+                                 stride_eta=1,  name="conv4")
+
+        pool4 = self._pooling_layer(conv4,
+                                    ksize_r=1,
+                                    ksize_xi=3,
+                                    ksize_eta=3,
+                                    stride_r=1,
+                                    stride_xi=1,
+                                    stride_eta=1, name="pool4")
+
+        print(pool4.get_shape)
+
+        flattened = tf.reshape(pool4,  [-1, self._reduce_dim(pool4)])
 
         fc1 = self._fc_layer(
-            flattened, self._reduce_dim(conv4), 2048, name="fc1")
+            flattened, self._reduce_dim(pool4), 2048, name="fc1")
         drop_out = tf.nn.dropout(fc1, self.keep_prob)
         fc2 = self._fc_layer(drop_out, 2048, 2048, name="fc2")
-        return self._out_layer(fc2, 2048, self.n_classes)
+
+        out = self._out_layer(fc2, 2048, self.n_classes)
+        print(out)
+        return out
 
     def _batch_factory(self):
         # get proteins feature file names and grid feature file names
