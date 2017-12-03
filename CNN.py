9197bb773324cd631e092efb100648be652dbd80
Thorn Hjalte Thorn Jakobsen <thornjakobsen@gmail.com>
2017-12-03 13:50:54 +0100

    first commit to branch

--- CNN.py
+++ CNN.py
@@ -31,8 +31,10 @@ FLAGS = None
 # Set flags
 flags = tf.app.flags
 
-flags.DEFINE_string("input_dir", "./data/atomistic_features_cubed_sphere_train/", "Input path")
-flags.DEFINE_float("test_set_fraction", 0.25,"Fraction of data set aside for testing")
+flags.DEFINE_string(
+    "input_dir", "./data/atomistic_features_cubed_sphere_train/", "Input path")
+flags.DEFINE_float("test_set_fraction", 0.25,
+                   "Fraction of data set aside for testing")
 flags.DEFINE_integer("validation_set_size", 10, "Size of validation set")
 flags.DEFINE_string("logdir", "tmp/summary/", "Path to summary files")
 flags.DEFINE_boolean("train", False, "Define if this is a training session")
@@ -40,6 +42,7 @@ flags.DEFINE_boolean("infer", False, "Define if this is a infering session")
 flags.DEFINE_boolean("batch_size", 10, "batch size to train on")
 
 FLAGS = flags.FLAGS
+
 
 class CNNModel(object):
     """deepnn builds the graph for a deep net for classifying residues.
@@ -55,9 +58,11 @@ class CNNModel(object):
     digits 0-20). keep_prob is a scalar placeholder for the probability of
     dropout.
     """
+
     def __init__(self):
         # internal setting
-        self.optimizer = tf.train.AdamOptimizer(0.001, beta1=0.9, beta2=0.999, epsilon=1e-08)
+        self.optimizer = tf.train.AdamOptimizer(
+            0.001, beta1=0.9, beta2=0.999, epsilon=1e-08)
         self.bias_initializer = tf.constant_initializer(0.0)
         self.shape = [-1, 6, 24, 38, 38, 2]
         self.n_classes = 21
@@ -65,7 +70,8 @@ class CNNModel(object):
         # placeholders
         self.labels = tf.placeholder(tf.float32, shape=[None, self.n_classes])
         self.x = tf.placeholder(tf.float32, shape=[None, 6, 24, 38, 38, 2])
-        self.keep_prob = tf.placeholder_with_default(tf.constant(1.0), shape=None)
+        self.keep_prob = tf.placeholder_with_default(
+            tf.constant(1.0), shape=None)
 
         # config
         self.batch_size = 10
@@ -75,39 +81,69 @@ class CNNModel(object):
         return tf.get_variable(name, shape, initializer=tf.truncated_normal_initializer(stddev=stddev, dtype=tf.float32))
 
     def _reduce_dim(self, x):
-            return reduce(operator.mul, x.shape.as_list()[1:], 1)
+        return reduce(operator.mul, x.shape.as_list()[1:], 1)
 
     def _fc_layer(self, input, channels_in, channels_out, name="fc"):
         with tf.variable_scope(name):
             W = self._weight_variable("weights", [channels_in, channels_out])
-            b = tf.get_variable("b", shape=[channels_out], initializer=self.bias_initializer, dtype=tf.float32)
+            b = tf.get_variable(
+                "b", shape=[channels_out], initializer=self.bias_initializer, dtype=tf.float32)
             return tf.nn.relu(tf.matmul(input, W) + b)
 
     def _out_layer(self, input, channels_in, channels_out, name="out"):
         with tf.variable_scope(name):
             W = self._weight_variable("weights", [channels_in, channels_out])
-            b = tf.get_variable("b", shape=[channels_out], initializer=self.bias_initializer, dtype=tf.float32)
+            b = tf.get_variable(
+                "b", shape=[channels_out], initializer=self.bias_initializer, dtype=tf.float32)
             return tf.matmul(input, W) + b
 
-    def _conv_layer(self, input, channels_in, channels_out, name="conv"):
+    def _conv_layer(self, input,
+                    channels_in,
+                    channels_out,
+                    name="conv",
+                    ksize_r=3,
+                    ksize_xi=3,
+                    ksize_eta=3,
+                    stride_r=2,
+                    stride_xi=2,
+                    stride_eta=2):
         with tf.variable_scope(name) as scope:
-            W = self._weight_variable("weights", [3, 3, 3, channels_in, channels_out])
-            b = tf.get_variable("b", shape=[channels_out], initializer=self.bias_initializer, dtype=tf.float32)
-            convolution = conv_spherical_cubed_sphere(input, W, strides=[1, 1, 1, 1, 1], padding="SAME", name=name)
-            activation = tf.nn.relu(convolution + b)
-            return avg_pool_spherical_cubed_sphere(activation, ksize=[1, 1, 3, 3, 1], strides=[1,1,2,2,1], padding="VALID")
+            W = self._weight_variable("weights", [ksize_r, ksize_xi, ksize_eta, channels_in, channels_out])
+            b = tf.get_variable(
+                "b", shape=[channels_out], initializer=self.bias_initializer, dtype=tf.float32)
+            convolution = conv_spherical_cubed_sphere(
+                input, W, strides=[1, stride_r, stride_xi, stride_eta, 1], padding="VALID", name=name)
+            return tf.nn.relu(convolution + b)
+
+    def _pooling_layer(self,
+                       input,
+                       name="pooling",
+                       ksize_r=3,
+                       ksize_xi=3,
+                       ksize_eta=3,
+                       stride_r=2,
+                       stride_xi=2,
+                       stride_eta=2):
+        with tf.variable_scope(name) as scope:
+            return avg_pool_spherical_cubed_sphere(input, ksize=[1, ksize_r, ksize_xi, ksize_eta, 1], strides=[1, stride_r, stride_xi, stride_eta, 1], padding="VALID")
 
     def _build_graph(self):
         # Reshape to use within a convolutional neural net.
         x = tf.reshape(self.x, shape=[-1, 6, 24, 38, 38, 2])
+
         conv1 = self._conv_layer(x, 2, 16,  name="conv1")
+        pool1 = self._pooling_layer(conv1)
+
         conv2 = self._conv_layer(conv1, 16, 32,  name="conv2")
+
         conv3 = self._conv_layer(conv2, 32, 64,  name="conv3")
+
         conv4 = self._conv_layer(conv3, 64, 128,  name="conv4")
 
         flattened = tf.reshape(conv4,  [-1, self._reduce_dim(conv4)])
 
-        fc1 = self._fc_layer(flattened, self._reduce_dim(conv4), 2048, name="fc1")
+        fc1 = self._fc_layer(
+            flattened, self._reduce_dim(conv4), 2048, name="fc1")
         drop_out = tf.nn.dropout(fc1, self.keep_prob)
         fc2 = self._fc_layer(drop_out, 2048, 2048, name="fc2")
         return self._out_layer(fc2, 2048, self.n_classes)
@@ -130,28 +166,28 @@ class CNNModel(object):
 
         # add the dataset X labels
         train_batch_factory.add_data_set("data",
-                                        protein_feature_filenames[:train_end],
-                                        grid_feature_filenames[:train_end])
+                                         protein_feature_filenames[:train_end],
+                                         grid_feature_filenames[:train_end])
 
         # add the dataset Y labels
         train_batch_factory.add_data_set("model_output",
-                                        protein_feature_filenames[:train_end],
-                                        key_filter=["aa_one_hot"])
+                                         protein_feature_filenames[:train_end],
+                                         key_filter=["aa_one_hot"])
 
         # create object from BatchFactory class
         validation_batch_factory = BatchFactory()
 
         # add the dataset X labels
         validation_batch_factory.add_data_set("data",
-                                            protein_feature_filenames[
-                                                validation_start:validation_end],
-                                            grid_feature_filenames[validation_start:validation_end])
+                                              protein_feature_filenames[
+                                                  validation_start:validation_end],
+                                              grid_feature_filenames[validation_start:validation_end])
 
         # add the dataset Y labels
         validation_batch_factory.add_data_set("model_output",
-                                            protein_feature_filenames[
-                                                validation_start:validation_end],
-                                            key_filter=["aa_one_hot"])
+                                              protein_feature_filenames[
+                                                  validation_start:validation_end],
+                                              key_filter=["aa_one_hot"])
 
         # create object from BatchFactory class
         test_batch_factory = BatchFactory()
@@ -170,12 +206,14 @@ class CNNModel(object):
 
     def _loss(self, logits, labels):
         with tf.name_scope('loss'):
-            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
+                labels=labels, logits=logits)
             return tf.reduce_mean(cross_entropy)
 
     def _accuracy(self, logits, labels):
         with tf.name_scope('accuracy'):
-            correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(self.labels, 1))
+            correct_prediction = tf.equal(
+                tf.argmax(logits, 1), tf.argmax(self.labels, 1))
             correct_prediction = tf.cast(correct_prediction, tf.float32)
             return tf.reduce_mean(correct_prediction)
 
@@ -195,10 +233,13 @@ class CNNModel(object):
         merged = tf.summary.merge_all()
 
         time_str = datetime.datetime.now().isoformat()
-        train_writer = tf.summary.FileWriter("./tmp/summary/train/{}".format(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')))
+        train_writer = tf.summary.FileWriter(
+            "./tmp/summary/train/{}".format(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')))
         train_writer.add_graph(tf.get_default_graph())
-        validation_writer = tf.summary.FileWriter("./tmp/summary/validation/{}".format(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')))
-        test_writer = tf.summary.FileWriter("./tmp/summary/test/{}".format(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')))
+        validation_writer = tf.summary.FileWriter(
+            "./tmp/summary/validation/{}".format(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')))
+        test_writer = tf.summary.FileWriter(
+            "./tmp/summary/test/{}".format(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')))
 
         ### TRAIN MODEL ###
         with session as sess:
@@ -208,18 +249,22 @@ class CNNModel(object):
             best_validation_accuracy = 0.0
             for step in range(self.max_steps):
                 batch, _ = batch_factory['train'].next(self.batch_size)
-                _, summary = sess.run([optimize_op, merged], feed_dict={self.x: batch["data"], self.labels: batch["model_output"], self.keep_prob: 0.5})
+                _, summary = sess.run([optimize_op, merged], feed_dict={self.x: batch[
+                                      "data"], self.labels: batch["model_output"], self.keep_prob: 0.5})
                 if step % 100 == 0:
                     # Train accuracy
-                    summary, train_accuracy = sess.run([merged, accuracy], feed_dict={self.x: batch["data"], self.labels: batch["model_output"], self.keep_prob: 1.0})
-                    print('step %d, training accuracy %g' % (step, train_accuracy))
+                    summary, train_accuracy = sess.run([merged, accuracy], feed_dict={self.x: batch[
+                                                       "data"], self.labels: batch["model_output"], self.keep_prob: 1.0})
+                    print('step %d, training accuracy %g' %
+                          (step, train_accuracy))
                     train_writer.add_summary(summary, step)
 
                     # Validation accuracy
                     batch, _ = batch_factory['validation'].next(10)
                     summary, validation_accuracy = sess.run([merged, accuracy],
-                    feed_dict={self.x: batch["data"], self.labels: batch["model_output"], self.keep_prob: 1.0})
-                    print('step %d, validation accuracy %g' %(step, validation_accuracy))
+                                                            feed_dict={self.x: batch["data"], self.labels: batch["model_output"], self.keep_prob: 1.0})
+                    print('step %d, validation accuracy %g' %
+                          (step, validation_accuracy))
                     validation_writer.add_summary(summary, step)
 
                     if validation_accuracy > best_validation_accuracy:
@@ -254,10 +299,11 @@ class CNNModel(object):
         # TODO: Here I need to have a tensor of size: (protein_size, patches, radius, xi, eta, channels) - can this work?
         print(predicted_logits) '''
         with session as sess:
-            predicted_logits = sess.run([self._probabilities(logits)], feed_dict={self.x: data})
-
+            predicted_logits = sess.run(
+                [self._probabilities(logits)], feed_dict={self.x: data})
 
         return predicted_logits
+
 
 def main(_):
     print("hej")
